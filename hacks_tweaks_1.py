24b70d7f2867052f047cd862091b1f97cf16bb40
Helena Cabanillas <helencabanillas@hotmail.com>
2018-05-10 22:06:07 -0400

    Initial commit

---
+++ hacks_tweaks_1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+#guess = input ('Which one of these is a fish? \
+#A) Whale B) Dolphin C) Shark D) Squid. Type A, B, C, or D')
+#check_guess(guess, 'C')
+
+
+#a = 5
+#print('hello')
+
+
+
+#def test(): print('bye')
+
+
+#def blah(): print('hi')
+
+
+#b = 1111
+
+#print (max(10, 11, 12, 13))
+
+print ('bang' .upper())
